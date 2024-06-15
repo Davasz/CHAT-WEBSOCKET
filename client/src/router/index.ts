23f6a08273f16a6504f5d3d412bcfd048c464538
Davasz <davidavi200615@gmail.com>
2024-06-15 11:42:51 -0300

    feat: setup front

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,14 +1,16 @@
-// import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
-// const router = createRouter({
-//   history: createWebHistory(import.meta.env.BASE_URL),
-//   routes: [
-//     {
-//       path: '/',
-//       name: 'home',
-//       component: HomeView
-//     },
-//   ]
-// })
+import HomeView from '../views/HomeView.vue'
 
-// export default router
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+  ]
+})
+
+export default router
